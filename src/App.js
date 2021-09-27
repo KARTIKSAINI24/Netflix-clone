7606fba10eeda102ab678b2ca70dac58d9713b25
KARTIKSAINI24 <sainikartik24@gmail.com>
2021-09-27 09:39:11 +0530

    started footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import Jumbotron from './components/jumbotron'
-import jumbodata from './fixtures/jumbo.json'
+import { JumbotronContainer } from "./containers/jumbotron"
 
 
 export default function App() {
   return (
-    <Jumbotron.Container>
-      {jumbodata.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{ item.title }</Jumbotron.Title>
-            <Jumbotron.SubTitle>{ item.subtitle }</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-          <Jumbotron.Image src={item.image} alt={item.alt}/>
-            </Jumbotron.Pane>
-          
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+    <JumbotronContainer/>
+  )
 }
 
  
