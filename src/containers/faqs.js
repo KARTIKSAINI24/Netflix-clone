cb8b6555d378280b7a1bbfbaae50105abe649690
KARTIKSAINI24 <sainikartik24@gmail.com>
2021-10-17 14:07:07 +0530

    completed opt-form

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Accordion } from '../components'
+import OptForm from '../components/opt-form'
 import faqsData from '../fixtures/faqs'
 
 export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently asked questions</Accordion.Title>
-            <Accordion.Frame>
+           
                 {faqsData.map((item) => (
                     <Accordion.Item key={item.id}>
                         <Accordion.Header>{item.header}</Accordion.Header>
                         <Accordion.Body>{item.body}</Accordion.Body>
                     </Accordion.Item>
                 ))}
-            </Accordion.Frame>
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it Now </OptForm.Button>
+                <OptForm.Text>Ready to watch? ENter your email to start your subscription</OptForm.Text>
+            </OptForm>
+
         </Accordion>
     )
 }
