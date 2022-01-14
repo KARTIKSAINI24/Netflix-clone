1b053c7ce6e71a30ff97d45954c095e0d2fb384a
KARTIKSAINI24 <sainikartik24@gmail.com>
2022-01-14 18:14:39 +0530

    started header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { FaqsContainer } from "./containers/faqs"
-import { FooterContainer } from "./containers/footer"
-import { JumbotronContainer } from "./containers/jumbotron"
-
+import * as ROUTES from './constants/routes'
+import { Route, BrowserRouter as Router } from "react-router-dom"
+import {Home, Browse, Signin, Signup} from './pages'
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer/>
-    <FooterContainer/>
-  </>)
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home/>
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup/>
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin/>
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse/>
+      </Route>
+    </Router>
+  )
 }
 
  
---
+++ src/pages/browse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Browse() {
+    return (
+        <div>
+            <p>hello Browse</p>
+        </div>
+    )
+}
+
+export default Browse
